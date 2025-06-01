99aeb82039531e6642e9d6dd9a3224a0d8292297
Uthmanade001 <Adeleyetemilade001@gmail.com>
2025-06-01 16:22:16 +0100

    new sh and yml

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-read -p "Enter your name: " user_name
+user_name="uthman"
+leader_name="temi"
+another_name="nurudeen"
+
 echo "Hello, $user_name! Welcome to the script."
-
-read -p "Enter your leader's name: " leader_name
 echo "Your leader's name is $leader_name."
-
-read -p "Enter another name: " another_name
 echo "You entered another name: $another_name.."
