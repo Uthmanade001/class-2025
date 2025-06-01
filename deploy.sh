7c28932f8214a074494b845f8c5d357d6fe5472f
Uthmanade001 <Adeleyetemilade001@gmail.com>
2025-06-01 14:49:42 +0100

    add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "my name is uthman"
+read -p "Enter your name: " name
+echo "my name is nurudeen"
+read -p "Enter your name: " name
+echo "leader name which is temi"
+read -p "Enter your name: " name 
