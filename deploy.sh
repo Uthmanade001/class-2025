83527a87ae813046f6a49634f85e71eb38666a20
Uthmanade001 <Adeleyetemilade001@gmail.com>
2025-06-01 14:54:45 +0100

    add changes to rep

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-echo "my name is uthman"
-read -p "Enter your name: " name
-echo "my name is nurudeen"
-read -p "Enter your name: " name
-echo "leader name which is temi"
-read -p "Enter your name: " name 
+read -p "Enter your name: " user_name
+echo "Hello, $user_name! Welcome to the script."
+
+read -p "Enter your leader's name: " leader_name
+echo "Your leader's name is $leader_name."
+
+read -p "Enter another name: " another_name
+echo "You entered another name: $another_name....."
